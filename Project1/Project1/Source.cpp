319919e9f3ed810845b426be109635b023fd35e9
Oleg Stalmakh <stelmah.taganrog@gmail.com>
2017-09-30 14:38:35 +0300

    test project for cirillic cymbols

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,30 @@
+/*
+  Иванов Иван Иванович
+  Лабораторная работа №1.
+  Вариант №1
+  Задание: Перевсти мили в сажени.
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+
+#define COEFFICIENT_OF_GRAVITY 9.8
+
+int main() {
+	// МОЙ код
+
+	if (true) {
+		printf("OK");
+
+		if (true) {
+			printf("&");
+		}
+	}
+	else {
+		printf("Bad");
+	}
+	_getch();
+	return 0;
+}
